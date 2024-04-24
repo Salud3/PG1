3ac89332b5e22de335a780a9770b90234e67ef82
Salud3 <78719641+Salud3@users.noreply.github.com>
2024-04-24 13:45:12 -0600

    Flauros Finished

--- Assets/Flauros/FlaurosCenter.cs
+++ Assets/Flauros/FlaurosCenter.cs
@@ -11,17 +11,24 @@ public class FlaurosCenter : MonoBehaviour
     Vector3[] vertices =
     {
         new Vector3(0,0,0),//0
-        new Vector3(0.5f,0,Mathf.Sqrt(3)/2),//1
-        new Vector3(-0.5f,0,Mathf.Sqrt(3)/2),//2
-        new Vector3(0.5f,Mathf.Sqrt(6)/3,Mathf.Sqrt(3)/2),//3
-        new Vector3(-0.5f,Mathf.Sqrt(6)/3,Mathf.Sqrt(3)/2),//4
-        new Vector3(0,Mathf.Sqrt(6)/3,0),//1
+        new Vector3(0.5f, 0 ,Mathf.Sqrt(3)/2),//1
+        new Vector3(-0.5f, 0 ,Mathf.Sqrt(3)/2),//2
+
+        new Vector3(0, Mathf.Sqrt(6)/3, (Mathf.Sqrt(3)/2)+.2886756f),//3
+        new Vector3(0.5f, Mathf.Sqrt(6)/3, .2886756f),//4
+        new Vector3(-0.5f, Mathf.Sqrt(6)/3,.2886756f)//5
+
 
     };
 
     int[] triangles = {0,1,2,//check //0
-                       
-
+                       3,4,5,
+                       0,4,1,
+                       2,5,0,
+                       1,4,3,
+                       1,3,2,
+                       2,3,5,
+                       0,5,4
     };
     void FlauroCenter()
     {
@@ -38,5 +45,7 @@ public class FlaurosCenter : MonoBehaviour
     void Start()
     {
         FlauroCenter();
+        transform.rotation = new Quaternion(0, 180, 0, 0);
+
     }
 }
